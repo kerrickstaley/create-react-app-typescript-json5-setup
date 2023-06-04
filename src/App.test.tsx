51ca9a5b3f11abeb9d06d4d280cdc7bb2ac5b5fc
Kerrick Staley <k@kerrickstaley.com>
2023-06-04 16:04:11 -0400

    Add failing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import en from './i18n/en.json5';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My cool application/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('json5 file is loaded', () => {
+  expect(en).toEqual({'APP.TITLE': 'My cool application'});
 });
